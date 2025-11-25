c504c0ebe7366098889795b7b09cbcee2f05c668
1540240353@qq.com <1540240353@qq.com>
2025-11-25 16:57:36 +0800

    feat: 构建后端映射

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,13 +1,28 @@
 from django.urls import path
-import view
-from . import views
-
-import myproject.myapp.view.user
+from .view import driver, line, schedule, user, vehicle
 
 # TODO: 映射肯定没写完
 urlpatterns = [
-    path('api/login/', view.user.login),
-    path('api/drivers/changeProfile', views.driver_change_profile),
-    path('api/admin/fetchAllDrivers', views.fetchAllDrivers),
-    path('api/admin/createDriver', views.add_driver)
+    path('api/user/login/', user.login),
+    path('api/user/changeProfile/', user.modifyUserInfo),
+    path('api/user/changePassword/', user.modifyUserPassword),
+
+    path('api/driver/changeInfo/', driver.driver_change_profile),
+    path('api/admin/drivers/fetchAll/', driver.fetchAllDrivers),
+    path('api/admin/drivers/add/', driver.add_driver),
+    path('api/driver/leave/add/', driver.addLeave),
+
+    path('api/admin/line/add/', line.addLine),
+    path('api/admin/line/fetchAll/', line.fetchAllLines),
+    path('api/admin/line/remove/', line.removeLine),
+
+    path('api/schedules/byDriver/', schedule.fetchSchedulesByDriver),
+    path('api/schedules/byVehicle/', schedule.fetchSchedulesByVehicle),
+    path('api/schedules/byLine/', schedule.fetchSchedulesByLine),
+
+    path('api/admin/vehicle/fetchAlltransferHistory/', vehicle.fetchAllTransferHistory),
+    path('api/admin/vehicle/fetchAll/', vehicle.fetchAllVehicles),
+    path('api/admin/vehicle/add/', vehicle.addVehicle),
+    path('api/admin/vehicle/remove/', vehicle.removeVehicle),
+    path('api/admin/vehicle/transfer/', vehicle.transferVehicle),
 ]
--- myproject/myapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.db import connection, transaction
-
-# TODO： 给这些分个类，放进views文件夹
